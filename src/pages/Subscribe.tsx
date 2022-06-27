7f60396356aac8ced781658f0e0b08251be49715
Sidneycarlinijr <sidney_carlini@hotmail.com>
2022-06-27 14:13:59 -0300

    Finalizacao do MVP

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
+    const navigate = useNavigate()
+    
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+
+    const [createSubscriber, { loading }] = useCreateSubscriberMutation()
+    
+    async function handleSubscribe(event: FormEvent) {
+        event.preventDefault();
+
+        await createSubscriber({
+            variables: {
+                name,
+                email,
+            }
+        })
+    navigate('/event')
+    }
 
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col itens-center">
@@ -20,7 +38,7 @@ export function Subscribe() {
                 <div className="p-8 bg-gray-700 border border-gray-500 rounded">
                     <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
-                    <form action="" className="flex flex-col gap-2 w-full">
+                    <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
                         <input
                             className="bg-gray-900 rounded px-5 h-14"
                             type="text"
@@ -36,7 +54,8 @@ export function Subscribe() {
 
                         <button
                             type="submit"
-                            className="nt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                            disabled={loading}
+                            className="nt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
                         >
                             Garantir minha vaga
                         </button>
