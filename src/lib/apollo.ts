f46ae4c2f936905194c38142f9d63cd96b4c6541
Sidneycarlinijr <sidney_carlini@hotmail.com>
2022-06-27 08:34:05 -0300

    Inicio do desenvolvimento p envio de dados de inscricao ao graphCMS

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4q23lep2p6w01xtevokejru/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization' : `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
